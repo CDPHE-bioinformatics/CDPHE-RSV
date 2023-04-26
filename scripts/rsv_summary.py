af6ce3f11aa413accd306d8123c46f68b5c3231d
Daniel Polanco <daniel.polanco@state.co.us>
2023-04-26 17:29:38 +0000

    Remove remnants of nextclade and pangolin

--- scripts/rsv_summary.py
+++ scripts/rsv_summary.py
@@ -75,60 +75,9 @@ def concat_percent_cvg(percent_cvg_file_list):
 
     return df
 
-def get_df_spike_mutations(variants_csv):
-    def get_sample_name(fasta_header):
-        sample_name = str(re.findall('CO-CDPHE-([0-9a-zA-Z_\-\.]+)', fasta_header)[0])
-        return sample_name 
-    
-    # read in variants file
-    variants = pd.read_csv(variants_csv, dtype = {'sample_name' : object})
-    variants = variants.drop(columns = 'fasta_header')
-
-    #### filter variants for spike protein varaints in rbd and pbcs #####
-    crit = variants.gene == 'S'
-    critRBD = (variants.codon_position >= 461) & (variants.codon_position <= 509)
-    critPBCS = (variants.codon_position >= 677) & (variants.codon_position <= 694)
-    crit732 = variants.codon_position == 732
-    crit452 = variants.codon_position == 452
-    crit253 = variants.codon_position == 253
-    crit13 = variants.codon_position == 13
-    crit145 = variants.codon_position == 145 # delta plus AY.4.2
-    crit222 = variants.codon_position == 222 # delta plus AY.4.2
-    critdel = variants.variant_name.str.contains('del') # mainly for 69/70 del
-
-    spike_variants_df = variants[crit & (critRBD | critPBCS | crit732 | critdel | crit452 | crit253 | crit13 | crit145 | crit222)]
-    spike_variants_df = spike_variants_df.reset_index(drop = True)
-
-    # generate a df of the sample and their spike variants
-    sample_name_list_variants = spike_variants_df.sample_name.unique().tolist()
-
-    df = pd.DataFrame()
-    sample_name_list = []
-    variant_name_list = []
-
-    seperator = '; '
-
-    for sample_name in sample_name_list_variants:
-        sample_name_list.append(sample_name)
-
-        crit = spike_variants_df.sample_name == sample_name
-        f = spike_variants_df[crit]
-        f = f.reset_index()
-
-        mutations = []
-        for row in range(f.shape[0]):
-            mutations.append(f.variant_name[row])
-        mutations_string = seperator.join(mutations)
-        variant_name_list.append(mutations_string)
-
-    df['sample_name'] = sample_name_list
-    df['spike_mutations'] = variant_name_list
-
-    return df
-
 def concat_results(sample_name_list, workbook_path, project_name, 
                    assembler_version,
-                   cov_out_df, percent_cvg_df, spike_variants_df):
+                   cov_out_df, percent_cvg_df):
 
     # set some functions for getting data formatted
     def get_sample_name_from_fasta_header(fasta_header):
@@ -150,22 +99,14 @@ def concat_results(sample_name_list, workbook_path, project_name,
     workbook = pd.read_csv(workbook_path, sep = '\t', dtype = {'sample_name' : object, 'hsn' : object})
     workbook = workbook.set_index('sample_name')
 
-    # read in nextclade csv
-    nextclade = nextclade.drop(columns = 'fasta_header')
-    nextclade = nextclade.set_index('sample_name')
-
     # set index on the samtools_df and percent_cvg_df and variants_df to prepare for joining
     cov_out_df = cov_out_df.set_index('sample_name')
     percent_cvg_df = percent_cvg_df.set_index('sample_name')
-    spike_variants_df = spike_variants_df.set_index('sample_name')
 
     # join
     j = df.join(workbook, how = 'left')
     j = j.join(percent_cvg_df, how = 'left')
     j = j.join(cov_out_df, how = 'left')
-    j = j.join(nextclade, how = 'left')
-    j = j.join(pangolin, how = 'left')
-    j = j.join(spike_variants_df, how = 'left')
     j = j.reset_index()
 
     # add fasta header
@@ -186,8 +127,7 @@ def concat_results(sample_name_list, workbook_path, project_name,
     columns.sort()
     primary_columns = ['hsn', 'sample_name', 'project_name', 'plate_name', 
                        'run_name', 'analysis_date', 'run_date', 'assembly_pass', 
-                 'percent_coverage', 'nextclade', 'pangolin_lineage', 
-                 'expanded_lineage', 'spike_mutations']
+                       'percent_coverage']
     for column in columns:
          if column not in primary_columns:
               primary_columns.append(column)
@@ -204,10 +144,10 @@ def make_wgs_horizon_output (results_df, project_name):
     results_df['Run_Date'] = str(date.today())
 
     # rename columns 
-    results_df = results_df.rename(columns = {'hsn' : 'accession_id', 'pango_designation_version' : 'pangoLEARN_version'})
+    results_df = results_df.rename(columns = {'hsn' : 'accession_id'})
 
-    col_order = ['accession_id', 'percent_coverage', 'pangolin_lineage', 'pangolin_version',
-                 'report_to_epi', 'Run_Date', 'pangoLEARN_version']
+    col_order = ['accession_id', 'percent_coverage',
+                 'report_to_epi', 'Run_Date']
     
     results_df = results_df[col_order]
 
@@ -239,8 +179,7 @@ if __name__ == '__main__':
                                 project_name = project_name,
                                 assembler_version = assembler_version,
                                 cov_out_df=cov_out_df,
-                                percent_cvg_df=percent_cvg_df, 
-                                spike_variants_df = spike_variants_df)
+                                percent_cvg_df=percent_cvg_df)
     
     # create wgs horizon output
     make_wgs_horizon_output(project_name = project_name,
